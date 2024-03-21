4617505b91b90f4a7be6ed3615357e2d0b4f7be8
Marianna Fernandes <145359517+mariannalf@users.noreply.github.com>
2024-03-20 20:35:35 -0500

    Add project files.

---
+++ IntegerExtensions.cs
@@ -0,0 +1,12 @@
+﻿public static class IntegerExtensions
+{
+    public static bool IsOdd(this int number)
+    {
+        return number % 2 != 0;
+    }
+
+    public static bool IsEven(this int number)
+    {
+        return number % 2 == 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 6, 7, 10, 20, 23, 30, 51, 100, 999, 10000 };
+
+        Console.WriteLine("Even numbers:");
+        foreach (int num in numbers)
+        {
+            if (num.IsEven())
+            {
+                Console.Write(num + " ");
+            }
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Odd numbers:");
+        foreach (int num in numbers)
+        {
+            if (num.IsOdd())
+            {
+                Console.Write(num + " ");
+            }
+        }
+        Console.WriteLine(); 
+    }
+}
